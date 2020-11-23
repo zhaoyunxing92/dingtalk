cf6106dbff2521341d64178ff3a0a5e211912dd1
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-11-23 13:32:50 +0800

    up:代码格式化

--- tests/dingtalk_test.go
+++ tests/dingtalk_test.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"github.com/zhaoyunxing92/dingtalk"
+	"log"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -14,4 +17,12 @@ func TestDingTalkGetToken(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(token)
+}
+
+func TestCurrentPath(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Logf("path:%s",strings.Replace(dir, "\\", "/", -1))
 }
