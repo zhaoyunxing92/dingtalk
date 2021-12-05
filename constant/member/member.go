7e0ef2c9656dddb6a4c8fc9cfd116f7be7644bd6
zhaoyunxing <zhaoyunxing@apache.org>
2021-12-05 22:13:47 +0800

    add:获取权限列表 (#44)
    * add:获取权限列表
    
    * 补充测试
    
    * add:添加权限

---
+++ constant/member/member.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 zhaoyunxing.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package member
+
+//Type 成员类型
+type Type string
+
+const (
+	//Org 企业
+	Org = Type("org")
+
+	//Department 部门
+	Department = Type("department")
+
+	//Conversation 群
+	Conversation = Type("conversation")
+
+	//User 用户
+	User = Type("user")
+)
