54d05786cfd001e16dd1f4b4f1749a9d6da8cc12
zhaoyunxing <zhaoyunxing@apache.org>
2022-07-03 22:42:09 +0800

    fix(#83):解决空问题

--- domain/message/oa_message_test.go
+++ domain/message/oa_message_test.go
@@ -45,3 +45,27 @@ func TestNewOaMessage(t *testing.T) {
 
 	t.Log(msg.String())
 }
+
+func TestNewOaMessage_Null_Rich(t *testing.T) {
+	msg := NewOaMessage("消息的头部标题", "FFBBBBBB", "http://dingtalk.com",
+		"http://dingtalk.com")
+
+	msg.Body.Title = "消息体"
+	msg.Body.Content = "消息体的内容，最多显示3行"
+	msg.Body.MediaId = "@lADOADmaWMzazQKA"
+	msg.Body.Forms = []Form{
+		{Key: "姓名", Value: "张三"},
+		{Key: "年龄", Value: "20"},
+		{Key: "身高", Value: "1.8m"},
+		{Key: "体重", Value: "70kg"},
+		{Key: "学历", Value: "本科"},
+		{Key: "爱好", Value: "打球、听歌"},
+	}
+
+	msg.Author = "李四"
+
+	msg.StatusBar.Value = "进行中"
+	msg.StatusBar.BackColor = "0xFFF65E5E"
+
+	t.Log(msg.String())
+}
